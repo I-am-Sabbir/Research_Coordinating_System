fd95b95f71717aba972043325955cc8ad6253d14
I-am-Sabbir <sabbir35-1979@diu.edu.bd>
2021-04-28 19:08:54 +0600

    test

---
+++ Framework/Services/IResearchSeminarService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Framework.Entities;
+
+namespace Framework.Services
+{
+    public interface IResearchSeminarService : IDisposable
+    {
+        (IList<ResearchSeminar> researchSeminars, int total, int totalDisplay) GetResearchSeminars(int pageindex,
+                                                                              int Pagesize,
+                                                                              string searchText,
+                                                                              string sortText);
+        void CreateResearchseminar(ResearchSeminar researchSeminar);
+        void EditResearchseminar(ResearchSeminar researchSeminar);
+        ResearchSeminar GetResearchseminar(int Id);
+        ResearchSeminar DeleteResearchSeminar(int Id);
+    }
+}
